6b8c6ece30119dc02ae48542b4b3e29acf11805a
AryanShelke <aryan.shelke.2003@gmail.com>
2025-04-05 21:30:07 -0700

    Optimize image loading on home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { Icons } from "@/components/icons";
 import { TransitionLink } from "@/components/TransitionLink";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { DATA } from "@/data/resume";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -22,10 +23,13 @@ export default function Home() {
       {/* Scrollable Content */}
       <div className="flex flex-col justify-center items-center min-h-screen overflow-auto">
         <div className="flex flex-col justify-center items-center max-w-3xl mx-auto p-6 gap-4">
-          <Avatar className="size-28 border">
-            <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
-            <AvatarFallback>{DATA.initials}</AvatarFallback>
-          </Avatar>
+          <Image
+            src={DATA.avatarUrl}
+            alt={DATA.name}
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
           <h1 className="text-5xl text-center font-bold">Aryan Shelke</h1>
           <h2 className="text-xl text-center text-bold">
             Full Stack Web Developer | M.S. Software Engineering @ SJSU
